a5531f3a0531b9b10d5a9b4db1e586e9129387a0
Étienne Charignon <etienne.charignon@gmail.com>
2016-02-09 18:22:21 +0100

    Met à joure les URL des draaf

--- indexation/download.sh
+++ indexation/download.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -ex
 
-wget -N -L -r -l6 --trust-server-names --accept-regex='.*/BSV-.*|.*\.pdf|.*/Bulletin-de-sante-du-vegetal.*$' http://draaf.auvergne-rhone-alpes.agriculture.gouv.fr/BSV-AUVERGNE-GRANDES-CULTURES-2015
+wget -N -L -r -l6 --trust-server-names --accept-regex='.*/BSV-.*|.*\.pdf|.*/Bulletin-de-sante-du-vegetal.*$' $1
+
+
+# find . -name "BSV-*"|grep -v ".*\.pdf"|xargs rm
+
+# http://draaf.auvergne-rhone-alpes.agriculture.gouv.fr/BSV-AUVERGNE-GRANDES-CULTURES-2015
