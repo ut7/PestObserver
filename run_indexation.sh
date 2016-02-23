609ad572944247c23c8c9463ecdcef0794bad956
Jonathan Perret <jonathan@ut7.fr>
2016-02-24 00:24:54 +0100

    Raffinage du script de lancement de l'indecation

--- run_indexation.sh
+++ run_indexation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 
-make -j 12 -i sql 2>&1|awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' >> indexation.log 2>&1
+(
+ echo ==============================================
+ make --debug=b sql 2>&1|awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }'
+) >> indexation.log
