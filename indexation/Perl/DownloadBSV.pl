e6f30fecf02254d3afbfe72b47cec1508dcfcf15
Jonathan Perret <jonathan@ut7.fr>
2016-03-09 00:28:39 +0100

    Enrichissement de la liste des BSV

--- indexation/Perl/DownloadBSV.pl
+++ indexation/Perl/DownloadBSV.pl
@@ -20,7 +20,8 @@ sub download {
 
   my $domain = URI->new($source_url)->host;
 
-  my @args = ('--timestamping',
+  my @args = ('--user-agent=BSV crawler for http://pestobserver.eu',
+              '--timestamping',
               '--recursive',
               '--level=6',
               '--trust-server-names',
@@ -42,7 +43,7 @@ sub download {
 }
 
 my $default_regex =
-    'BSV|Archives-BSV|ARCHIVES-BSV|\.pdf|Grandes-cultures'
+    'BSV|Archives-BSV|ARCHIVES-BSV|\.pdf|Grandes-[Cc]ultures'
   . '|Arboriculture|Cultures-legumieres|Lin-oleagineux|Noix|Tabac'
   . '|Viticulture|Chataigne|Horticulture|Prairie|[Bb]ulletin|Bilan_'
   . '|Nouvel-article|Campagne|GRANDES-CULTURES|VIGNE|LEGUMES'
@@ -51,9 +52,11 @@ my $default_regex =
 download($default_regex, 'http://draaf.alsace-champagne-ardenne-lorraine.agriculture.gouv.fr/Surveillance-des-organismes');
 
 download($default_regex, 'http://draaf.aquitaine-limousin-poitou-charentes.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+
 download($default_regex
   . '|bulletins-de-sante-du-vegetal-bsv',
   'http://www.aquitainagri.fr/menu-horizontal/publications/bulletins-de-sante-du-vegetal-bsv.html');
+
 download($default_regex
   . '|synagri/(arboriculture|pomme-de-terre|grandes-cultures'
   . '|fraise|maraichage-pomme-de-terre|prairies|chataigne|noix'
@@ -68,13 +71,28 @@ download($default_regex
   . '|Legumes-frais|Legumes-industrie|Pommes-de-terre|Cultures-ornementales',
   'http://draaf.bretagne.agriculture.gouv.fr/Bulletin-de-Sante-du-Vegetal-BSV');
 
-download($default_regex, 'http://draaf.centre-val-de-loire.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+download($default_regex,
+  'http://draaf.centre-val-de-loire.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
 
-download($default_regex, 'http://draaf.corse.agriculture.gouv.fr/Les-bulletins-de-sante-du-vegetal');
-download($default_regex.'|Midi-Pyrenees|Languedoc-Roussillon|Lozere', 'http://draaf.languedoc-roussillon-midi-pyrenees.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
-download($default_regex.'|Nord-Pas-de-Calais|Picardie', 'http://draaf.nord-pas-de-calais-picardie.agriculture.gouv.fr/Bulletins-de-sante-du-vegetal-BSV');
-download($default_regex, 'http://draaf.normandie.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
-download($default_regex.'|2016|2015', 'http://draaf.paca.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal-BSV');
+download($default_regex,
+  'http://draaf.corse.agriculture.gouv.fr/Les-bulletins-de-sante-du-vegetal');
+
+download($default_regex
+  . '|/20[0-9][0-9]|Midi-Pyrenees|Languedoc-Roussillon|Lozere',
+  'http://draaf.languedoc-roussillon-midi-pyrenees.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+
+download($default_regex
+  . '|Nord-Pas-de-Calais|Picardie|Legumes|Petits-fruits',
+  'http://draaf.nord-pas-de-calais-picardie.agriculture.gouv.fr/Bulletins-de-sante-du-vegetal-BSV');
+
+download($default_regex
+  . '|Campagne|Cereales|Horticulture|Legumes|Oleagineux|Lin',
+  'http://draaf.normandie.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+
+download($default_regex
+  . '|/20[0-9][0-9]|Cultures-Mar|Oleiculture|Riz|Jardins|Vigne',
+  'http://draaf.paca.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal-BSV');
+
 download($default_regex, 'http://draaf.pays-de-la-loire.agriculture.gouv.fr/Derniers-BSV');
 download($default_regex, 'http://www.paysdelaloire.chambagri.fr/menu/vegetal/surveillance-biologique-du-territoire/bsv-grandes-cultures.html');
 download($default_regex, 'http://driaaf.ile-de-france.agriculture.gouv.fr/Epidemiosurveillance-et-Bulletin');
