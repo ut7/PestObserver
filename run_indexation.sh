f685d8c29eadaf5122c64d0945f151a7f638e778
Jonathan Perret <jonathan@ut7.fr>
2016-03-08 17:17:37 +0100

    Amélioration du script de lancement de l'indexation

--- run_indexation.sh
+++ run_indexation.sh
@@ -1,6 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+logfile=indexation-$(date +%Y%m%d_%H%M%d).log
+
+ln -sf "$logfile" indexation.log
 
 (
- echo ==============================================
- make --debug=b sql 2>&1|awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }'
-) >> indexation.log
+ make --debug=b "$@" 2>&1|awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }'
+) >> "$logfile"
