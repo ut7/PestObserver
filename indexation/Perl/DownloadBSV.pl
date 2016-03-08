472ceda4d01b97e8279d17d66302ae57b0d51602
Jonathan Perret <jonathan@ut7.fr>
2016-03-09 00:01:50 +0100

    Enrichissement de la liste des BSV

--- indexation/Perl/DownloadBSV.pl
+++ indexation/Perl/DownloadBSV.pl
@@ -3,50 +3,78 @@ use autodie;
 use warnings;
 use File::Path qw(make_path);
 use Getopt::Long 2.39;
+use URI;
 
 my $only = '.*';
-GetOptions('only=s' => \$only) or die;
+my $verbose = 0;
+GetOptions('only=s' => \$only, 'verbose' => \$verbose) or die;
 
-my $REPORTS="data/downloadedBSV";
+my $REPORTS='data/downloadedBSV';
 make_path($REPORTS);
 chdir $REPORTS;
 
 sub download {
-  my ($regex, $source_url) = @_;
+  my ($regex, $source_url, @extra_options) = @_;
 
   return unless $source_url =~ /$only/;
 
-  printf STDERR "Downloading BSV from: %s", $source_url;
-  my $ret = system("wget", "--timestamping", "--relative", "--recursive", "--level=6",
-    "--trust-server-names", "--accept-regex=$regex",
-    "--no-verbose",
-    $source_url);
-  die "wget failed ($ret)" unless $ret == 0;
+  my $domain = URI->new($source_url)->host;
+
+  my @args = ('--timestamping',
+              '--recursive',
+              '--level=6',
+              '--trust-server-names',
+              '--domains='.$domain,
+              '--accept-regex='.$regex,
+              '--reject-regex=login');
+
+  push @args, '--no-verbose' unless $verbose;
+
+  push @args, @extra_options;
+
+  say STDERR "Downloading BSV from: $source_url";
+
+  my $ret = system 'wget', @args, $source_url;
+
+  die '[ERROR] Failed to start wget' if $ret < 0;
+  die sprintf('[ERROR] wget terminated with signal %s', $ret & 127) if $ret & 127;
+  warn sprintf('[WARNING] wget exited with code %d', $ret >> 8) if $ret != 0;
 }
 
 my $default_regex =
     'BSV|Archives-BSV|ARCHIVES-BSV|\.pdf|Grandes-cultures'
   . '|Arboriculture|Cultures-legumieres|Lin-oleagineux|Noix|Tabac'
-  . '|Viticulture|Chataigne|Horticulture|Prairie|Bulletin|Bilan_'
+  . '|Viticulture|Chataigne|Horticulture|Prairie|[Bb]ulletin|Bilan_'
   . '|Nouvel-article|Campagne|GRANDES-CULTURES|VIGNE|LEGUMES'
   . '|HORTICULTURE|HOUBLON|ZNA|TABAC|Pour-les|/bsv-';
 
-download($default_regex, "http://draaf.alsace-champagne-ardenne-lorraine.agriculture.gouv.fr/Surveillance-des-organismes");
+download($default_regex, 'http://draaf.alsace-champagne-ardenne-lorraine.agriculture.gouv.fr/Surveillance-des-organismes');
 
-download($default_regex, "http://draaf.aquitaine-limousin-poitou-charentes.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal");
+download($default_regex, 'http://draaf.aquitaine-limousin-poitou-charentes.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
 download($default_regex
   . '|bulletins-de-sante-du-vegetal-bsv',
-  "http://www.aquitainagri.fr/menu-horizontal/publications/bulletins-de-sante-du-vegetal-bsv.html");
+  'http://www.aquitainagri.fr/menu-horizontal/publications/bulletins-de-sante-du-vegetal-bsv.html');
+download($default_regex
+  . '|synagri/(arboriculture|pomme-de-terre|grandes-cultures'
+  . '|fraise|maraichage-pomme-de-terre|prairies|chataigne|noix'
+  . '|zone-non-agricole|tabac|viticulture|horticulture|legumes-de-plein-champ)|TECHPJPARCODE',
+  'http://www.limousin.synagri.com/synagri/bulletins-de-sante-du-vegetal-00013060');
 
-download($default_regex, "http://draaf.auvergne-rhone-alpes.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal");
-download($default_regex, "http://draaf.bourgogne-franche-comte.agriculture.gouv.fr/Bulletins-de-sante-du-vegetal-BSV");
-download($default_regex, "http://draaf.bretagne.agriculture.gouv.fr/Bulletin-de-Sante-du-Vegetal-BSV");
-download($default_regex, "http://draaf.centre-val-de-loire.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal");
-download($default_regex, "http://draaf.corse.agriculture.gouv.fr/Les-bulletins-de-sante-du-vegetal");
-download("$default_regex|Midi-Pyrenees|Languedoc-Roussillon|Lozere", "http://draaf.languedoc-roussillon-midi-pyrenees.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal");
-download("$default_regex|Nord-Pas-de-Calais|Picardie", "http://draaf.nord-pas-de-calais-picardie.agriculture.gouv.fr/Bulletins-de-sante-du-vegetal-BSV");
-download($default_regex, "http://draaf.normandie.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal");
-download("$default_regex|2016|2015", "http://draaf.paca.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal-BSV");
-download($default_regex, "http://draaf.pays-de-la-loire.agriculture.gouv.fr/Derniers-BSV");
-download($default_regex, "http://www.paysdelaloire.chambagri.fr/menu/vegetal/surveillance-biologique-du-territoire/bsv-grandes-cultures.html");
-download($default_regex, "http://driaaf.ile-de-france.agriculture.gouv.fr/Epidemiosurveillance-et-Bulletin");
+download($default_regex, 'http://draaf.auvergne-rhone-alpes.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+
+download($default_regex, 'http://draaf.bourgogne-franche-comte.agriculture.gouv.fr/Bulletins-de-sante-du-vegetal-BSV');
+
+download($default_regex
+  . '|Legumes-frais|Legumes-industrie|Pommes-de-terre|Cultures-ornementales',
+  'http://draaf.bretagne.agriculture.gouv.fr/Bulletin-de-Sante-du-Vegetal-BSV');
+
+download($default_regex, 'http://draaf.centre-val-de-loire.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+
+download($default_regex, 'http://draaf.corse.agriculture.gouv.fr/Les-bulletins-de-sante-du-vegetal');
+download($default_regex.'|Midi-Pyrenees|Languedoc-Roussillon|Lozere', 'http://draaf.languedoc-roussillon-midi-pyrenees.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+download($default_regex.'|Nord-Pas-de-Calais|Picardie', 'http://draaf.nord-pas-de-calais-picardie.agriculture.gouv.fr/Bulletins-de-sante-du-vegetal-BSV');
+download($default_regex, 'http://draaf.normandie.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal');
+download($default_regex.'|2016|2015', 'http://draaf.paca.agriculture.gouv.fr/Bulletin-de-sante-du-vegetal-BSV');
+download($default_regex, 'http://draaf.pays-de-la-loire.agriculture.gouv.fr/Derniers-BSV');
+download($default_regex, 'http://www.paysdelaloire.chambagri.fr/menu/vegetal/surveillance-biologique-du-territoire/bsv-grandes-cultures.html');
+download($default_regex, 'http://driaaf.ile-de-france.agriculture.gouv.fr/Epidemiosurveillance-et-Bulletin');
